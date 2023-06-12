d525407b154942f07e5663a427a0d4883193a9c2
David <chuckbucker94@gmail.com>
2023-06-12 19:42:51 +0300

    "users" Database Implementation

---
+++ app/src/main/java/com/example/term_project/DBHelper.java
@@ -0,0 +1,55 @@
+package com.example.term_project;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private final Context context;
+    private static final String DATABASE_NAME = "UserDatabase.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "users";
+    private static final String COLUMN_ID = "id_users";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_PASS = "pass";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_EMAIL + " TEXT,"
+                + COLUMN_PASS + " TEXT" + ")";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void addUser(String name, String email, String password) {//igor identical to ai
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_NAME, name);
+        cv.put(COLUMN_EMAIL, email);
+        cv.put(COLUMN_PASS, password);
+
+        long result = db.insert(TABLE_NAME, null, cv);
+    }
+}
+
